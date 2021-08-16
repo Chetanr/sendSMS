5e8d900277de247f45b05a7c3790308d70d37b22
chetanrevappa <dgrchetan@gmail.com>
2021-08-16 12:53:09 +1000

    changes to app

--- src/History.js
+++ src/History.js
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import axios from "axios";
-
-// const useStyles = makeStyles((theme) => ({
-//   seeMore: {
-//     marginTop: theme.spacing(3),
-//   },
-// }));
 
 function History() {
   const [history, getHistory] = useState([]);
@@ -21,33 +14,14 @@ function History() {
   }, []);
 
   const getData = async () => {
-    // await axios
-    //   .get("https://ue1um90kg3.execute-api.us-east-1.amazonaws.com/getHistory")
-    //   .then(
-    //     (response) => {
-    //       getHistory(response.data.Items);
-    //     },
-    //     (error) => {
-    //       return error;
-    //     }
-    //   );
     await axios
-      .post(
-        "https://8leafevc72.execute-api.us-east-1.amazonaws.com/send",
-        null,
-        {
-          params: {
-            phone: "+61460542611",
-            message: "ioeos",
-          },
-        }
-      )
+      .get("https://ue1um90kg3.execute-api.us-east-1.amazonaws.com/getHistory")
       .then(
         (response) => {
-          console.log(response);
+          getHistory(response.data.Items);
         },
         (error) => {
-          console.log(error);
+          return error;
         }
       );
   };
@@ -57,10 +31,18 @@ function History() {
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Phone Number</TableCell>
-            <TableCell>Message</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Delivery Status</TableCell>
+            <TableCell>
+              <h3>Phone Number</h3>
+            </TableCell>
+            <TableCell>
+              <h3>Message</h3>
+            </TableCell>
+            <TableCell>
+              <h3>Date</h3>
+            </TableCell>
+            <TableCell>
+              <h3>Delivery Status</h3>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
--- src/Send.js
+++ src/Send.js
@@ -7,65 +7,62 @@ function Send() {
   const [message, setMessage] = useState("");
 
   const sendData = async () => {
-    // const headers = {
-    //   phone_number: phone,
-    //   message: message,
-    // };
     await axios
       .post(
-        "https://8leafevc72.execute-api.us-east-1.amazonaws.com/send",
-        // null,
+        "https://8leafevc72.execute-api.us-east-1.amazonaws.com/send?",
         {
-          //   headers: {
-          "Access-Control-Allow-Origin": "*",
-          phone: "+61460542611",
-          message: "ioeos",
-          //   },
+          headers: { "Content-Type": "application/json" },
+        },
+        {
+          params: {
+            phone: phone,
+            message: message,
+          },
         }
       )
       .then(
         (response) => {
-          console.log(response);
+          response.data === "SUCCESSFUL"
+            ? alert("Message sent successfully.!")
+            : alert("Mesage failed to send.!");
         },
         (error) => {
-          console.log(error);
+          return error;
         }
       );
   };
 
   const handlePhoneChange = (event) => {
     setPhone(event.target.value);
-    console.log(phone);
   };
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
-    console.log(message);
   };
 
   return (
     <div>
-      <form onSubmit={() => sendData()}>
-        <InputLabel>Phone Number :</InputLabel>
-        <br />
-        <FormControl>
-          <TextField error variant="outlined" onChange={handlePhoneChange} />
-        </FormControl>
+      {/* <form onSubmit={() => sendData()}> */}
+      <InputLabel>Phone Number :</InputLabel>
+      <br />
+      <FormControl>
+        <TextField error variant="outlined" onChange={handlePhoneChange} />
+      </FormControl>
 
-        <br />
-        <br />
-        <InputLabel>Message :</InputLabel>
-        <br />
-        <FormControl>
-          <TextField error variant="outlined" onChange={handleMessageChange} />
-        </FormControl>
+      <br />
+      <br />
+      <InputLabel>Message :</InputLabel>
+      <br />
+      <FormControl>
+        <TextField error variant="outlined" onChange={handleMessageChange} />
+      </FormControl>
 
-        <br />
-        <br />
-        <Button type="submit" variant="contained" color="primary">
-          Send Message
-        </Button>
-      </form>
+      <br />
+      <br />
+      <Button onClick={() => sendData()} variant="contained" color="primary">
+        Send Message
+      </Button>
+      {/* </form> */}
     </div>
   );
 }
